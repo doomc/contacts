4eda48243aaccf8396fe663b873ef8c8cadc9160
XWDdoomc <714024089@qq.com>
2016-05-16 09:01:40 +0800

    Initial Commit

---
+++ demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by 熊维东 on 16/5/16.
+//  Copyright © 2016年 熊维东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
